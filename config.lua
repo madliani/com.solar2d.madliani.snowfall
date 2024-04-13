d40e2711c64a1f2c20534aaa240fec3f4dcf0aae
madliani <madliani@hotmail.com>
2024-04-13 21:46:40 +0400

    fix: game icons changed

--- config.lua
+++ config.lua
@@ -1,23 +1,10 @@
---
--- For more information on config.lua see the Project Configuration Guide at:
--- https://docs.coronalabs.com/guide/basics/configSettings
---
-
 application =
 {
 	content =
 	{
 		width = 320,
-		height = 480, 
+		height = 480,
 		scale = "letterbox",
-		fps = 60,
-		
-		--[[
-		imageSuffix =
-		{
-			    ["@2x"] = 2,
-			    ["@4x"] = 4,
-		},
-		--]]
-	},
+		fps = 60
+	}
 }
