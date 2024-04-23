1cd43582716ebaf3b167669ac4ea3a655b1a3af8
madliani <madliani@hotmail.com>
2024-04-23 19:46:38 +0400

    feat: resources file added

---
+++ resources.lua
@@ -0,0 +1,12 @@
+return {
+    images =
+    {
+        background = "Assets/background.png",
+        snowflake = "Assets/snowflake.png"
+    },
+    sounds =
+    {
+        gameSound = "Sounds/the_field_of_dreams.mp3",
+        startSound = "Sounds/the_field_of_dreams.mp3"
+    }
+}
