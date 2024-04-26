536281c119f25ba701c8343ca6a0ca0a5128e77b
madliani <madliani@hotmail.com>
2024-04-26 15:19:44 +0400

    refactor: image and sound resources refactored

--- resources.lua
+++ resources.lua
@@ -1,12 +1,12 @@
 return {
     images =
     {
-        background = "Assets/background.png",
-        snowflake = "Assets/snowflake.png"
+        background = "Assets/Images/background.png",
+        snowflake = "Assets/Images/snowflake.png"
     },
     sounds =
     {
-        gameSound = "Sounds/the_field_of_dreams.mp3",
-        startSound = "Sounds/the_field_of_dreams.mp3"
+        gameSound = "Assets/Sounds/the_field_of_dreams.mp3",
+        startSound = "Assets/Sounds/the_field_of_dreams.mp3"
     }
 }
