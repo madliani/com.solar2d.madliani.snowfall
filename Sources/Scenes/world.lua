0c63cfdab044663c69fdd7940da6534c79577ea7
madliani <madliani@hotmail.com>
2024-05-25 14:50:14 +0400

    fix: world formatted

--- Sources/Scenes/world.lua
+++ Sources/Scenes/world.lua
@@ -1,14 +1,11 @@
-local Resources = require "resources"
-
 local Background = require "Libraries.Engine.Widgets.background"
-local Scene = require "Libraries.Engine.Core.scene"
-
 local Blizzard = require "Sources.Objects.blizzard"
 local Count = require "Sources.Objects.count"
 local Counter = require "Sources.Objects.counter"
+local Resources = require "resources"
+local Scene = require "Libraries.Engine.Core.scene"
 
 local background = Background(Resources.Images.background)
-
 local count = Count(0)
 local counter = Counter(count)
 local blizzard = Blizzard(counter)
