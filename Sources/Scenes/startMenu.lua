f4cbf649e8068cf81d8f291d6e64d3c170bd6566
madliani <madliani@hotmail.com>
2024-05-25 16:29:18 +0400

    fix: start menu formatted

--- Sources/Scenes/startMenu.lua
+++ Sources/Scenes/startMenu.lua
@@ -1,12 +1,9 @@
-local Resources = require "resources"
-
 local Background = require "Libraries.Engine.Widgets.background"
-local Scene = require "Libraries.Engine.Core.scene"
-
-local Snowfall = require "Sources.snowfall"
-
 local Heading = require "Sources.Objects.heading"
 local Menu = require "Sources.Objects.menu"
+local Resources = require "resources"
+local Scene = require "Libraries.Engine.Core.scene"
+local Snowfall = require "Sources.snowfall"
 
 local heading = Heading "Snowfall"
 local background = Background(Resources.Images.background)
