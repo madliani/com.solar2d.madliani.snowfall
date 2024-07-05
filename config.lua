f9618f60584a141244a93e77716e860b11680656
madliani <madliani@hotmail.com>
2024-07-05 19:18:30 +0400

    fix: config fixed

--- config.lua
+++ config.lua
@@ -1,8 +1,15 @@
 application = {
     content = {
-        fps = 60,
         height = 480,
         scale = "letterbox",
         width = 320,
+        xAlign = "center",
+        yAlign = "center",
+        fps = 60,
+
+        imageSuffix = {
+            ["@2x"] = 2.000,
+            ["@4x"] = 4.000,
+        },
     },
 }
