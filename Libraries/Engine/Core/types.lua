f23c011c40e6f090c00591ed559280acb9a46610
madliani <madliani@hotmail.com>
2024-07-12 18:22:25 +0400

    feat: playable class added

--- Libraries/Engine/Core/types.lua
+++ Libraries/Engine/Core/types.lua
@@ -4,6 +4,12 @@
 ---@alias GroupIndexIdentificator string
 ---@alias GroupIndexes table<GroupIndexIdentificator, GroupIndex>
 
+---@class Playable
+---@field create fun(group: Group)
+---@field destroy fun()
+---@field show fun()
+---@field hide fun()
+
 ---@class Group
 ---@field insert fun(self: Group, index: GroupIndex, item: any)
 ---@field remove fun(self: Group, item: GroupIndex)
