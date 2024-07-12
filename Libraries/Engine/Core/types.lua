92e9ea1c204626e508181a74257f22d46c465d7d
madliani <madliani@hotmail.com>
2024-07-12 18:04:14 +0400

    fix: group type fixed

--- Libraries/Engine/Core/types.lua
+++ Libraries/Engine/Core/types.lua
@@ -1,5 +1,5 @@
 ---@meta
 
 ---@class Group
----@field insert fun(self: Group, item: any)
----@field remove fun(item: any)
+---@field insert fun(self: Group, index: number, item: any)
+---@field remove fun(self: Group, item: number)
