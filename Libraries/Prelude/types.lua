c7a8912e26e52091d92f09c807641fb33e36482a
madliani <madliani@hotmail.com>
2024-07-13 18:42:04 +0400

    fix: prototype type fixed

--- Libraries/Prelude/types.lua
+++ Libraries/Prelude/types.lua
@@ -4,20 +4,20 @@
 ---@alias Function fun(...: Arguments?): any
 ---@alias Class fun(...: Arguments?): Object
 ---@alias Identificator string
+---@alias FieldId Identificator
 
----@alias PrototypeIdentificator Identificator
----@alias PrototypeAttributeIdentificator Identificator
----@alias PrototypeMethodIdentificator Identificator
+---@alias PrototypeId Identificator
+---@alias PrototypeFieldId FieldId
 ---@alias PrototypeAttribute any
 ---@alias PrototypeMethod fun(self: PrototypeSelf, ...: Arguments?): any
----@alias PrototypeAttributes table<PrototypeAttributeIdentificator, PrototypeAttribute>
----@alias PrototypeMethods table<PrototypeMethodIdentificator, PrototypeMethod>
----@alias PrototypeSelf table<string, any>
+---@alias PrototypeAttributes table<PrototypeFieldId, PrototypeAttribute>
+---@alias PrototypeMethods table<PrototypeFieldId, PrototypeMethod>
+---@alias PrototypeSelf table<PrototypeFieldId, any>
 ---@alias PrototypeInitializer fun(attributes: PrototypeAttributes, ...: Arguments?)
 ---@alias PrototypeFinalizer fun(attributes: PrototypeAttributes, methods: PrototypeMethods?)
 
 ---@class Prototype
----@field id PrototypeIdentificator
+---@field id PrototypeId
 ---@field attributes PrototypeAttributes
 ---@field methods PrototypeMethods
 ---@field initializer PrototypeInitializer?
