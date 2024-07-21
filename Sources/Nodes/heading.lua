9e07c96c571fe5d831184ed1aa6657e3d48ae184
madliani <madliani@hotmail.com>
2024-07-21 15:39:44 +0400

    refactor: heading refactored

--- Sources/Nodes/heading.lua
+++ Sources/Nodes/heading.lua
@@ -6,17 +6,85 @@ local Resources = require "resources"
 local Title = require "Libraries.Engine.Core.title"
 local display = require "display"
 
-local architectsDaughter = Font(Resources.Fonts.main)
-local color = Color(1, 1, 1)
-local coordinate = Coordinate(display.contentCenterX, display.contentCenterY - 200)
-local size = 48
+---@class Heading
+---@field create fun(group: table)
+---@field destroy fun()
+---@field show fun()
+---@field hide fun()
+---@field updateText fun(newText: string)
 
----@param text string
-local Heading = function(text)
-    local title = Title(text, architectsDaughter, color, size)
+---@alias HeadingClass fun(text: string): Heading
+---@alias HeadingIdentificator string
 
-    ---@class Heading
-    return Label(title, coordinate)
-end
+---@class HeadingAttributes
+---@field label Label | nil
+
+---@class HeadingSelf: HeadingAttributes, HeadingMethods
+
+---@class HeadingMethods
+---@field create fun(self: HeadingSelf, group: table)
+---@field destroy fun(self: HeadingSelf)
+---@field show fun(self: HeadingSelf)
+---@field hide fun(self: HeadingSelf)
+---@field updateText fun(self: HeadingSelf, newText: string)
+
+---@alias HeadingInitializer fun(attributes: HeadingAttributes, text: string)
+---@alias HeadingFinalizer fun(attributes: HeadingAttributes)
+
+---@class HeadingPrototype
+---@field id HeadingIdentificator
+---@field attributes HeadingAttributes
+---@field methods HeadingMethods
+---@field initializer HeadingInitializer?
+---@field finalizer HeadingFinalizer?
+
+---@alias HeadingMetaclass fun(prototype: HeadingPrototype): HeadingClass
+
+---@type HeadingMetaclass
+local Metaclass = require "Libraries.Prelude.metaclass"
+
+local Heading = Metaclass {
+    id = "heading",
+
+    attributes = {
+        label = nil,
+    },
+
+    methods = {
+        create = function(self, group)
+            self.label.create(group)
+        end,
+
+        destroy = function(self)
+            self.label.destroy()
+        end,
+
+        show = function(self)
+            self.label.show()
+        end,
+
+        hide = function(self)
+            self.label.hide()
+        end,
+
+        updateText = function(self, newText)
+            self.label.updateText(newText)
+        end,
+    },
+
+    initializer = function(attributes, text)
+        local architectsDaughterFont = Font(Resources.Fonts.main)
+        local blackColor = Color(1, 1, 1)
+        local coordinate = Coordinate(display.contentCenterX, display.contentCenterY - 200)
+        local size = 48
+        local title = Title(text, architectsDaughterFont, blackColor, size)
+
+        attributes.label = Label(title, coordinate)
+    end,
+
+    finalizer = function(attributes)
+        attributes.label = nil
+    end,
+}
 
 return Heading
