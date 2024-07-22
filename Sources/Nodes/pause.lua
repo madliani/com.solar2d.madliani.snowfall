349b6e852a7c40f2d1f3252eb2adea14532ca0cb
madliani <madliani@hotmail.com>
2024-07-22 21:34:22 +0400

    refactor: pause refactored

--- Sources/Nodes/pause.lua
+++ Sources/Nodes/pause.lua
@@ -7,17 +7,77 @@ local Snowfall = require "Sources.snowfall"
 local display = require "display"
 local paths = require "paths"
 
-local coordinate = Coordinate(50, display.contentHeight + 25)
-local size = Size(25, 25)
-local path = Resources.Images.transparrentPause
+---@class Pause: Image
 
-local event = Event(function()
-    Snowfall.pause(paths.Scenes.pauseMenu)
-end, "tap")
+---@alias PauseClass fun(): Pause
+---@alias PauseIdentificator string
 
-local Pause = function()
-    ---@class Pause
-    return Image(path, size, coordinate, event)
-end
+---@class PauseAttributes
+---@field image Image | nil
+
+---@class PauseSelf: PauseAttributes, PauseMethods
+
+---@class PauseMethods
+---@field create fun(self: PauseSelf, group: table)
+---@field destroy fun(self: PauseSelf)
+---@field show fun(self: PauseSelf)
+---@field hide fun(self: PauseSelf)
+
+---@alias PauseInitializer fun(attributes: PauseAttributes)
+---@alias PauseFinalizer fun(attributes: PauseAttributes)
+
+---@class PausePrototype
+---@field id PauseIdentificator
+---@field attributes PauseAttributes
+---@field methods PauseMethods
+---@field initializer PauseInitializer?
+---@field finalizer PauseFinalizer?
+
+---@alias PauseMetaclass fun(prototype: PausePrototype): PauseClass
+
+---@type PauseMetaclass
+local Metaclass = require "Libraries.Prelude.metaclass"
+
+local Pause = Metaclass {
+    id = "pause",
+
+    attributes = {
+        image = nil,
+    },
+
+    methods = {
+        create = function(self, group)
+            self.image.create(group)
+        end,
+
+        destroy = function(self)
+            self.image.destroy()
+        end,
+
+        show = function(self)
+            self.image.show()
+        end,
+
+        hide = function(self)
+            self.image.hide()
+        end,
+    },
+
+    initializer = function(attributes)
+        local coordinate = Coordinate(50, display.contentHeight + 25)
+        local size = Size(25, 25)
+        local path = Resources.Images.transparrentPause
+
+        local event = Event(function()
+            Snowfall.pause(paths.Scenes.pauseMenu)
+        end, "tap")
+
+        attributes.image = Image(path, size, coordinate, event)
+    end,
+
+    finalizer = function(attributes)
+        attributes.image = nil
+    end,
+}
 
 return Pause
