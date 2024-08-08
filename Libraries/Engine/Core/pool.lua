f0a7e44148f36250322a8048908d5bda7c52f7b0
madliani <madliani@hotmail.com>
2024-08-08 22:44:31 +0400

    refactor: pool refactored

--- Libraries/Engine/Core/pool.lua
+++ Libraries/Engine/Core/pool.lua
@@ -1,67 +1,98 @@
----@param objects unknown[]
-local Pool = function(objects)
-    local pool = objects
+---@class Pool
+---@field create fun(group: table<any, any>)
+---@field destroy fun()
+---@field show fun()
+---@field hide fun()
 
-    local function destroy()
-        if pool ~= nil and #pool > 0 then
-            for i = 1, #pool, 1 do
-                pool[i].destroy()
-            end
-        end
-    end
+---@alias PoolClass fun(pool: any[]): Pool
+---@alias PoolIdentificator string
 
-    ---@param group table
-    local function create(group)
-        if pool ~= nil and #pool > 0 then
-            for i = 1, #pool, 1 do
-                if group ~= nil then
-                    pool[i].create(group)
-                else
-                    pool[i].create()
+---@class PoolAttributes
+---@field pool any[] | nil
+
+---@class PoolSelf: PoolAttributes, PoolMethods
+
+---@class PoolMethods
+---@field create fun(self: PoolSelf, group: table)
+---@field destroy fun(self: PoolSelf)
+---@field show fun(self: PoolSelf)
+---@field hide fun(self: PoolSelf)
+
+---@alias PoolInitializer fun(attributes: PoolAttributes, objects: any[])
+---@alias PoolFinalizer fun(attributes: PoolAttributes)
+
+---@class PoolPrototype
+---@field id PoolIdentificator
+---@field attributes PoolAttributes
+---@field methods PoolMethods
+---@field initializer PoolInitializer?
+---@field finalizer PoolFinalizer?
+
+---@alias PoolMetaclass fun(prototype: PoolPrototype): PoolClass
+
+---@type PoolMetaclass
+local Metaclass = require "Libraries.Prelude.metaclass"
+
+local Pool = Metaclass {
+    id = "pool",
+
+    attributes = {
+        pool = nil,
+    },
+
+    methods = {
+        create = function(self, group)
+            if self.pool ~= nil and #self.pool > 0 then
+                for i = 1, #self.pool, 1 do
+                    if group ~= nil then
+                        self.pool[i].create(group)
+                    else
+                        self.pool[i].create()
+                    end
                 end
             end
-        end
-    end
+        end,
 
-    local function show()
-        if pool ~= nil and #pool > 0 then
-            for i = 1, #pool, 1 do
-                local object = pool[i]
-
-                if object.show ~= nil then
-                    object.show()
-                elseif object.resume ~= nil then
-                    object.resume()
+        destroy = function(self)
+            if self.pool ~= nil and #self.pool > 0 then
+                for i = 1, #self.pool, 1 do
+                    self.pool[i].destroy()
                 end
             end
-        end
-    end
+        end,
 
-    local function hide()
-        if pool ~= nil and #pool > 0 then
-            for i = 1, #pool, 1 do
-                local object = pool[i]
+        show = function(self)
+            if self.pool ~= nil and #self.pool > 0 then
+                for i = 1, #self.pool, 1 do
+                    local object = self.pool[i]
 
-                if object.hide ~= nil then
-                    object.hide()
-                elseif object.pause ~= nil then
-                    object.pause()
+                    if object.show ~= nil then
+                        object.show()
+                    elseif object.resume ~= nil then
+                        object.resume()
+                    end
                 end
             end
-        end
-    end
+        end,
 
-    ---@class Pool
-    ---@field create function
-    ---@field destroy function
-    ---@field hide function
-    ---@field show function
-    return {
-        create = create,
-        destroy = destroy,
-        hide = hide,
-        show = show,
-    }
-end
+        hide = function(self)
+            if self.pool ~= nil and #self.pool > 0 then
+                for i = 1, #self.pool, 1 do
+                    local object = self.pool[i]
+
+                    if object.hide ~= nil then
+                        object.hide()
+                    elseif object.pause ~= nil then
+                        object.pause()
+                    end
+                end
+            end
+        end,
+    },
+
+    initializer = function(attributes, pool)
+        attributes.pool = pool
+    end,
+}
 
 return Pool
