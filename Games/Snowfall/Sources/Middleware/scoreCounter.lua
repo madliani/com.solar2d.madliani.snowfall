e2cb6ba00dc8973af6327860381ad891f6d9a1a5
madliani <madliani@hotmail.com>
2024-08-15 20:22:49 +0400

    refactor: score counter refactored

--- Games/Snowfall/Sources/Middleware/scoreCounter.lua
+++ Games/Snowfall/Sources/Middleware/scoreCounter.lua
@@ -1,20 +1,56 @@
----@param score Score
-local ScoreCounter = function(score)
-    local function inc()
-        score.inc()
-    end
+---@class ScoreCounter: Score
 
-    local function dec()
-        score.dec()
-    end
+---@alias ScoreCounterClass fun(score: Score): ScoreCounter
+---@alias ScoreCounterIdentificator string
 
-    ---@class ScoreCounter
-    ---@field dec function
-    ---@field inc function
-    return {
-        dec = dec,
-        inc = inc,
-    }
-end
+---@class ScoreCounterAttributes
+---@field score Score | nil
+
+---@class ScoreCounterSelf: ScoreCounterAttributes, ScoreCounterMethods
+
+---@class ScoreCounterMethods
+---@field inc fun(self: ScoreCounterSelf)
+---@field dec fun(self: ScoreCounterSelf)
+
+---@alias ScoreCounterInitializer fun(attributes: ScoreCounterAttributes, score: Score)
+---@alias ScoreCounterFinalizer fun(attributes: ScoreCounterAttributes)
+
+---@class ScoreCounterPrototype
+---@field id ScoreCounterIdentificator
+---@field attributes ScoreCounterAttributes
+---@field methods ScoreCounterMethods
+---@field initializer ScoreCounterInitializer?
+---@field finalizer ScoreCounterFinalizer?
+
+---@alias ScoreCounterMetaclass fun(prototype: ScoreCounterPrototype): ScoreCounterClass
+
+---@type ScoreCounterMetaclass
+local Metaclass = require "Libraries.Prelude.metaclass"
+
+local ScoreCounter = Metaclass {
+    id = "score_counter",
+
+    attributes = {
+        score = nil,
+    },
+
+    methods = {
+        inc = function(self)
+            if self.score ~= nil then
+                self.score.inc()
+            end
+        end,
+
+        dec = function(self)
+            if self.score ~= nil then
+                self.score.dec()
+            end
+        end,
+    },
+
+    initializer = function(attributes, score)
+        attributes.score = score
+    end,
+}
 
 return ScoreCounter
